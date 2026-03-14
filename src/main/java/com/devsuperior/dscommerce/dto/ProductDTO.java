8657e9fe27dae05a5832aeec033e32a257883ff5
Pedro Lauton <lautonpedro@gmail.com>
2026-03-14 18:36:14 -0300

    Ajustes em DTOs

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -1,10 +1,7 @@
 package com.devsuperior.dscommerce.dto;
 
 import com.devsuperior.dscommerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +17,13 @@ public class ProductDTO {
     @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
     @NotBlank(message = "Campo requerido")
     private String description;
-    
+
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
-    @NotEmpty(message = "Deve conter 1 categoria.")
+    @NotEmpty(message = "Deve conter pelo menos uma categoria.")
     private List<CategoryDTO> categories = new ArrayList<>();
 
     public ProductDTO() {
