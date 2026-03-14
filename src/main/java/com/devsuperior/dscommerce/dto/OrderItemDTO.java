d424002e26983f22a5f4556390a1dbb3cd64944d
Pedro Lauton <lautonpedro@gmail.com>
2026-03-14 18:02:43 -0300

    Acrescentando imgUrl em OrderItemDTO

--- src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -8,12 +8,14 @@ public class OrderItemDTO {
     private String name;
     private Double price;
     private Integer quantity;
+    private String imagUrl;
 
-    public OrderItemDTO(Long id, String name, Double price, Integer quantity) {
+    public OrderItemDTO(Long id, String name, Double price, Integer quantity,String imagUrl) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.imagUrl = imagUrl;
     }
 
     public OrderItemDTO(OrderItem orderItem) {
@@ -21,6 +23,7 @@ public class OrderItemDTO {
         this.name = orderItem.getProduct().getName();
         this.price = orderItem.getProduct().getPrice();
         this.quantity = orderItem.getQuantity();
+        this.imagUrl = orderItem.getProduct().getImgUrl();
     }
 
     public Long getId() {
@@ -41,5 +44,9 @@ public class OrderItemDTO {
 
     public Double getSubTotal() {
         return price * quantity;
+    }
+
+    public String getImagUrl() {
+        return imagUrl;
     }
 }
